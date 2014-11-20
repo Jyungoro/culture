90bd7ee0d93fba0a6cbc57c0d1cda0c8af61c848
Ryu <Ryu@example.com>
2014-11-20 20:20:12 +0900

    SeedDada

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name: "Hun94Rie", password: "04120823", password_confirmation: "04120823")
+User.create(name: "okanopon", password: "pontonpan", password_confirmation: "pontonpan")
+User.create(name: "aya111love", password: "worldwide1love24", password_confirmation: "worldwide1love24")
+User.create(name: "ayacooo", password: "ranayako", password_confirmation: "ranayako")
+User.create(name: "manchi1255", password: "918baseball", password_confirmation: "918baseball")
+User.create(name: "soredekimarija", password: "butanko", password_confirmation: "butanko")
+User.create(name: "sh0104vi", password: "garam466", password_confirmation: "garam466")
+User.create(name: "onigiri", password: "renren1013", password_confirmation: "renren1013")
+User.create(name: "kincho", password: "kinchoshinai", password_confirmation: "kinchoshinai")
+User.create(name: "yamanaka", password: "yaman", password_confirmation: "yaman")
